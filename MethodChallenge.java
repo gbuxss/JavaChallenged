b3380f88e5861a469d8cc4e12596267d3ce0cfbd
gbuxss <jeevandhamala095@gmail.com>
2024-08-12 20:15:50 -0500

    create a method challenge 1- classify position based on scores

---
+++ MethodChallenge.java
@@ -0,0 +1,42 @@
+public class MethodChallenge {
+
+    public static void main(String[] args) {
+
+        int score = calculateHIghScorePosition(1500);
+        displayHighScorePosition("John", score);
+
+        score = calculateHIghScorePosition(676);
+        displayHighScorePosition("Jack", score);
+
+        score = calculateHIghScorePosition(100);
+        displayHighScorePosition("Jill", score);
+
+        score = calculateHIghScorePosition(20);
+        displayHighScorePosition("Bob", score);
+
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+
+        System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score list");
+
+    }
+
+    public static int calculateHIghScorePosition(int playerScore) {
+
+        int position = 4;
+        if (playerScore >= 1000) {
+            position = 1;
+
+        } else if (playerScore >= 500) {
+            position = 2;
+
+        } else if (playerScore >= 100) {
+            position = 3;
+
+        }
+        return position;
+    }
+
+}
