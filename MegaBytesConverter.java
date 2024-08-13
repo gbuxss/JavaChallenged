3d3a4f6efc3c1f9e2c5096430ffe9c080bbcbf34
gbuxss <jeevandhamala095@gmail.com>
2024-08-13 12:02:38 -0500

    challenge 3: MegaBytes Converter

---
+++ MegaBytesConverter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MegaBytesConverter {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of KiloBytes: ");
+        int kiloBytes = input.nextInt();
+        printMegaBytesAndKiloBytes(kiloBytes);
+        input.close();
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        if (kiloBytes < 0 ) {
+
+            System.out.println("Invalid Value");
+        }else {
+
+            int kilobytes = kiloBytes % 1024;
+            int megabytes = kiloBytes / 1024;
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + kilobytes + " KB");
+        }
+    }
+}
