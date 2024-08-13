3093dbef685bb563f8e07fabc5154370d5f7117d
gbuxss <jeevandhamala095@gmail.com>
2024-08-12 21:02:45 -0500

    challenge 2-- console beginner number check program

---
+++ MethodChallenges/CheckNumber.java
@@ -0,0 +1,28 @@
+package MethodChallenges;
+
+/****
+ * Program to check number is positive, negative or zero based on user input
+
+ *****/
+public class CheckNumber {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-9);
+        checkNumber(0);
+    }
+
+
+    //method to check number condition
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+
+        } else if (number < 0) {
+            System.out.println("negative");
+
+        } else if (number == 0) {
+            System.out.println("zero");
+        }
+    }
+}
