c777c538441b12af60ab0c269327ef7f425c6fc2
gbuxss <jeevandhamala095@gmail.com>
2024-08-16 16:26:17 -0500

    add composition project

---
+++ Composition/Ceiling.java
@@ -0,0 +1,20 @@
+package Composition;
+
+public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int paintedColor, int height) {
+        this.paintedColor = paintedColor;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
